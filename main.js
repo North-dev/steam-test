6251116c2435929be263c5e57986e467e651c93c
North-dev <57631249+North-dev@users.noreply.github.com>
2022-05-25 08:52:40 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
 function cb() {
-	var x = document.getElementById("x1");
-	navigator.clipboard.writeText(x.value);
-	alert("Copied the text: " + x.value);
 	window.location.replace("https://steamcommunity.com/id/htgshawn/");
 }

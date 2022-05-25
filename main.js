66fbed8b8088892ccc48783edf3e1af4cb8e83c2
North-dev <57631249+North-dev@users.noreply.github.com>
2022-05-25 08:48:01 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 function cb() {
 	var x = document.getElementById("x1");
-	x.select();
 	navigator.clipboard.writeText(x.value);
 	alert("Copied the text: " + x.value);
 	window.location.replace("https://steamcommunity.com/id/htgshawn/");

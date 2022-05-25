fa57924124b1e9f43ca3d53144eb38b8187821bf
North-dev <57631249+North-dev@users.noreply.github.com>
2022-05-25 08:44:53 +0200

    Update main.js

--- main.js
+++ main.js
@@ -3,5 +3,5 @@ function cb() {
 	x.select();
 	navigator.clipboard.writeText(x.value);
 	alert("Copied the text: " + x.value);
-	window.location.replace("http://www.w3schools.com");
+	window.location.replace("https://steamcommunity.com/id/htgshawn/");
 }

630c2e18294796df51333d429559da35eb4aa598
North-dev <57631249+North-dev@users.noreply.github.com>
2022-05-25 08:43:04 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-var x = document.getElementById("x1");
-x.select();
-navigator.clipboard.writeText(x.value);
-alert("Copied the text: " + x.value);
+function cb() {
+	var x = document.getElementById("x1");
+	x.select();
+	navigator.clipboard.writeText(x.value);
+	alert("Copied the text: " + x.value);
+}
